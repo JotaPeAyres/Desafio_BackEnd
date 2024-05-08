51be9f9505f71639c5ca4a26a026a204842b3f8b
João Pedro Moreno Ayres <jp.ayres2601@gmail.com>
2024-05-08 01:17:59 -0300

    Add docker compose

--- MotorcycleRental/src/MotorcycleRental.Bussiness/RabbitMq/RabbiMqConfig.cs
+++ MotorcycleRental/src/MotorcycleRental.Bussiness/RabbitMq/RabbiMqConfig.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation.Validators;
+using Microsoft.Extensions.Configuration;
 using MotorcycleRental.Bussiness.Interfaces;
 using RabbitMQ.Client;
 using System;
@@ -11,9 +12,15 @@ namespace MotorcycleRental.Bussiness.RabbitMq
 {
     public class RabbiMqConfig : IRabbitMqConfig
     {
+        private readonly IConfiguration _configuration;
+
+        public RabbiMqConfig(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public IModel ConnectChannel()
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var factory = new ConnectionFactory() { HostName = _configuration["rabbitMQConfig"], Port = 5672 };
             var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
             {
